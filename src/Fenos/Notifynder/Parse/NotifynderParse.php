a076394bf6360019d792b2030796b9655452aec4
Tosho Trajanov <TsTrv@users.noreply.github.com>
2014-12-02 18:56:21 +0100

    Use json objects as extra fields
    Use JSON formatted objects as an extra field in order to store multiple records

--- src/Fenos/Notifynder/Parse/NotifynderParse.php
+++ src/Fenos/Notifynder/Parse/NotifynderParse.php
@@ -1,77 +1,9 @@
-<?php namespace Fenos\Notifynder\Parse;
+<?php use Fenos\Notifynder\Parse\NotifynderParse;
 
-use Illuminate\Database\Eloquent\Collection;
-
-/**
- *
- * Class parse used on collection. It permit to decode the special
- * values inserted on the notification
- *
- * @package Fenos\Notifynder\Parse
- */
-class NotifynderParse
+class NotifynderParser extends NotifynderParse
 {
 
-    /**
-     * Regex to get values between curly brachet {$value}
-     */
-    const RULE = '/\{(.+?)(?:\{(.+)\})?\}/';
-
-    /**
-     * Parse special value from a noficiation
-     * Model or a collection
-     *
-     * @param $item
-     * @param $extra
-     * @return mixed
-     */
-    public function parse($item,$extra)
-    {
-        $body = $item->body->text;
-
-        $valuesToParse = $this->getValues($body);
-
-        if ($valuesToParse > 0)
-        {
-            return $this->replaceSpecialValues($valuesToParse,$item,$body,$extra);
-        }
-    }
-
-    /**
-     * Replace specialValues
-     *
-     * @param $valuesToParse
-     * @param $item
-     * @param $body
-     * @param $extra
-     * @return mixed
-     */
-    public function replaceSpecialValues($valuesToParse,$item,$body,$extra)
-    {
-        foreach($valuesToParse as $value)
-        {
-            // get an array of nested values, means that there is a relations
-            // in progress
-            $value_user = explode('.', $value);
-
-            // get name relations
-            $relation = array_shift($value_user);
-
-            if ( strpos($value, $relation . '.') !== false ) // yes
-            {
-                $body = $this->insertValuesRelation($value_user, $relation,$body,$item);
-            }
-
-            if( ! is_null($extra))
-            {
-                $body = $this->replaceExtraParameter($value,$body,$extra);
-            }
-        }
-
-        return $body;
-    }
-
-    /**
+	/**
      * Replace relations values
      *
      * @param $value_user
@@ -80,15 +12,18 @@ class NotifynderParse
      * @param $item
      * @return mixed
      */
-    public function insertValuesRelation($value_user, $relation,$body, $item)
+    public function insertValuesRelation($value_user, $relation, $body, $item)
     {
         foreach($value_user as $value)
         {
-            $body = preg_replace(
-                "{{" . $relation . "." . $value . "}}",
-                $item[$relation][$value],
-                $body
-            );
+            if($relation!='extra'){
+                $body = preg_replace(
+                    "{{" . $relation . "." . $value . "}}",
+                    $item[$relation][$value],
+                    $body
+                );  
+            }
+            
         }
 
         return $body;
@@ -103,24 +38,15 @@ class NotifynderParse
      */
     public function replaceExtraParameter($value,$body,$extra)
     {
-        return $item['body']['text'] = preg_replace(
-            "{{".$value."}}",
-            $extra,
-            $body
-        );
-    }
-
-    /**
-     * Get the values between {}
-     * and return an array of it
-     *
-     * @param $body
-     * @return mixed
-     */
-    public function getValues($body)
-    {
-        $values = [];
-        preg_match_all(self::RULE, $body, $values);
-        return $values[1];
+        $item['body']['text'] = $body;
+        $extra=json_decode($extra);
+        foreach ($extra as $key => $extraValue) {
+            $item['body']['text'] = preg_replace(
+                "{{extra.".$key."}}",
+                $extraValue,
+                $item['body']['text']
+            );
+        }
+        return $item['body']['text'];
     }
 }
